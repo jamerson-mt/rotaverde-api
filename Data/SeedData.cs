ae3e983869bb27c96b84d89d97420995a6d183fb
jamerson-mt <jjamersonmt@gmail.com>
2026-03-19 21:43:37 -0300

    feat: seed configuration create defaults users roles professor

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -30,22 +30,45 @@ namespace RotaVerdeAPI.Data
 
                 // Criar usuário padrão com a role "professor"
                 var defaultProfessorEmail = "professor@rotaverde.com";
-                var defaultProfessorPassword = "Professor123!";
-                if (userManager.FindByEmailAsync(defaultProfessorEmail).Result == null)
+                var defaultProfessorPassword = "@Password123";
+                string defaultAdminEmail = "jjamersonmt@gmail.com";
+                string defaultAdminPassword = "@Password123";
+
+                if (userManager.FindByEmailAsync(defaultProfessorEmail).Result == null) // Verifica se o usuário já existe
                 {
                     var professorUser = new ApplicationUser
                     {
-                        UserName = defaultProfessorEmail,
+                        UserName = "lincoln",
                         Email = defaultProfessorEmail,
-                        EmailConfirmed = true
+                        EmailConfirmed = false,
                     };
 
-                    var result = userManager.CreateAsync(professorUser, defaultProfessorPassword).Result;
-                    if (result.Succeeded)
+                    var adminUser = new ApplicationUser
                     {
-                        userManager.AddToRoleAsync(professorUser, "professor").Wait();
+                        UserName = "jjamersonmt",
+                        Email = defaultAdminEmail,
+                        EmailConfirmed = false,
+                    };
+
+
+
+                    var result = userManager
+                        .CreateAsync(professorUser, defaultProfessorPassword)// Cria o usuário com a senha especificada
+                        .Result; // Aguarda a conclusão da tarefa
+                    if (result.Succeeded) // Se a criação do usuário foi bem-sucedida, atribui a role "professor"
+                    {
+                        userManager.AddToRoleAsync(professorUser, "professor").Wait(); // Aguarda a conclusão da tarefa de atribuição de role
+                    }
+
+                    var adminResult = userManager
+                        .CreateAsync(adminUser, defaultAdminPassword) // Cria o usuário admin com a senha especificada
+                        .Result; // Aguarda a conclusão da tarefa
+                    if (adminResult.Succeeded) // Se a criação do usuário admin foi bem-sucedida, atribui a role "professor"
+                    {         
+                        userManager.AddToRoleAsync(adminUser, "professor").Wait(); // Aguarda a conclusão da tarefa de atribuição de role para o admin
                     }
                 }
+
             }
         }
     }
